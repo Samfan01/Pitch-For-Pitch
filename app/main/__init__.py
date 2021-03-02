afcdb37458622dfd5c370c0ced357ffcc7ac2f0e
samfan01 <dsamfan@gmail.com>
2021-03-02 12:45:13 +0300

    created the base html file for template inheritance

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,6 +1,11 @@
 from flask import Blueprint
 main = Blueprint('main',__name__)
 from . import views,error
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -10,6 +15,8 @@ def create_app(config_name):
     
     #Initializing flask extensions
     bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
     
     # Registering the blueprint
     from .main import main as main_blueprint
