8f9d450d76a7a27e741653b211addf02677c202e
samfan01 <dsamfan@gmail.com>
2021-03-03 10:57:37 +0300

    added the mail response functionality

--- app/__init__.py
+++ app/__init__.py
@@ -26,6 +26,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
     
     #will add the views and forms
     
--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 export SECRET_KEY=Samfan01
+export MAIL_USERNAME=dsamfan@gmail.com
+export MAIL_PASSWORD=Therockers
 
 python3.6 manage.py server
